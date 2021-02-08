3616cd7bc2cc618ca62ac081ee9f844557718a52
3mawy <m.3mawyy@gmail.com>
2021-02-08 04:37:55 +0200

    added new components

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,7 +2,7 @@ import HighlightsItems from "../components/Highlightsitems.js/HighlightsItems";
 import HighlightsBrand from "../components/HighlightsBrand/HighlightsBrand";
 import CardsList from "../components/Cards/CardsList";
 import {Container} from "react-bootstrap";
-import Title from "../components/CommonComponents/Title";
+import Title from "../components/CommonComponents/Title/Title";
 import Banner from "../components/CommonComponents/Banner/Banner";
 
 const AboutUs = () => {
--- src/pages/ItemsList.js
+++ src/pages/ItemsList.js
@@ -2,6 +2,8 @@ import Pagination from "../components/Pagination/Pagination";
 import CardsList from "../components/Cards/CardsList";
 import {Col, Container, Row} from "react-bootstrap";
 import Sidebar from "../components/Sidebar/Sidebar";
+import Title from "../components/CommonComponents/Title/Title";
+import Banner from "../components/CommonComponents/Banner/Banner";
 
 const ItemsList = () => {
     return (
@@ -11,6 +13,7 @@ const ItemsList = () => {
                 <Sidebar/>
                 </Col>
                 <Col lg={9}>
+                    <Banner title = "Chicken" desc="Have A Taste Of The Best Fried Chicken Sandwiches!"/>
                     <CardsList row_size={4}></CardsList>
                 </Col>
             </Row>
